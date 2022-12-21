ec148ae001e3640d151412257d0825624c05dc4f
Lucas Porto <lucasrodriguesporto@gmail.com>
2022-12-20 21:38:23 -0300

    fix(adjust-js-songs): fix

--- Aula3_SonsAnimais/main.js
+++ Aula3_SonsAnimais/main.js
@@ -1,15 +1,27 @@
-function tocaSom(){
-    document.querySelector('.som__burro').play();
+
+// Função para adicionar som nos cards, utilizando o id do audio
+function tocaSom(elementoAudio){
+    document.querySelector(elementoAudio).play();
 }
+
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+// Realizando a contagem dos elementos da lista de Teclas
+let contador = 0; 
 
-while(contador < listaDeTeclas.length ){
-    listaDeTeclas[contador].onclick = tocaSom;
+
+while(contador < listaDeTeclas.length){
+
+    const tecla = listaDeTeclas[contador];
+    const classeSom = tecla.classList[1];
+    // Template String
+    const classeAudio = `#${classeSom}`;
+
+    // Ao clicar em alguma das teclas, realizar a ação
+    tecla.onclick = function (){
+        tocaSom(classeAudio);
+    }
 
     contador = contador +1 ;
-
-    console.log("Esse é o valor do contador: " + contador);
 }
